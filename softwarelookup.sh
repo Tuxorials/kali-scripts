02c1cb1a33e52ad2dd80e5b4dbdebdf221f33932
Tuxorials <61655859+Tuxorials@users.noreply.github.com>
2021-03-14 15:43:33 +0100

    Made it a bit nicer to look at
    CLI color (whoo fancy)

--- softwarelookup.sh
+++ softwarelookup.sh
@@ -1,8 +1,20 @@
+#!/bin/zsh
+clear
 IP=$1
+figlet KScripts
+echo "                             by Tuxorials"
+echo -e "\e[1mScanning Webserver\e[0m"
 nikto=$(nikto -h $IP)
 serverversion=$(echo $nikto | tail -n +8 | head -n +1)
-gobu=$(gobuster dir -q -u http://$IP -w /usr/share/dirb/wordlists/common.txt -x .php,.html,.txt,.zip,.jpg,.js,.css | tail -n +16 | head -n -4)
-echo "Server Software:"
-echo $serverversion
-echo "Maybe relevant sites:"
-echo $gobu
+clear
+figlet KScripts
+echo "                             by Tuxorials"
+echo -e "\e[1mScanning Commonly used Sites\e[0m"
+gobu=$(gobuster dir -q -u http://$IP -w /usr/share/dirb/wordlists/common.txt -x .php,.html,.txt,.zip,.jpg,.js,.css)
+clear
+figlet KScripts
+echo "                             by Tuxorials"
+echo -e "\e[1;91mServer Software:"
+echo -e "\e[1;92m"+$serverversion
+echo -e "\e[1;91mMaybe relevant sites:"
+echo -e "\e[1;92m"+ $gobu

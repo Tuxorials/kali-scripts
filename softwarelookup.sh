17c9150e7eac2c282255acc6a56c6ff3ec1f44b6
Tuxorials <61655859+Tuxorials@users.noreply.github.com>
2021-03-14 15:28:28 +0100

    Update softwarelookup.sh

--- softwarelookup.sh
+++ softwarelookup.sh
@@ -1,7 +1,7 @@
 IP=$1
 nikto=$(nikto -h $IP)
 serverversion=$(echo $nikto | tail -n +8 | head -n +1)
-gobu=$(gobuster dir -q -u http://10.10.10.8 -w /usr/share/dirb/wordlists/common.txt -x .php,.html,.txt,.zip,.jpg,.js,.css | tail -n +16 | head -n -4 | uniq)
+gobu=$(gobuster dir -q -u http://$IP -w /usr/share/dirb/wordlists/common.txt -x .php,.html,.txt,.zip,.jpg,.js,.css | tail -n +16 | head -n -4)
 echo "Server Software:"
 echo $serverversion
 echo "Maybe relevant sites:"
